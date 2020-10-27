181d3f0c1ae6573348ac849c7f87ecac5d87c957
fynjy <antony.ets@yandex.ru>
2020-10-27 18:05:44 +0300

    next

--- src/test.php
+++
@@ -1,3 +0,0 @@
-<?php
-$b= new \lliytnik\eventbus\EventBus(new \lliytnik\eventbus\transport\FileTransport('.'));
-$b->send(new \lliytnik\eventbus\events\LeadEvent(['data'=>['id'=>1,'method'=>'test']]));
